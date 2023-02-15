c013b0c090c30ff72d2b30292d3c82db8609a856
Cameron Cohen <cameronco@bbd.co.za>
2023-02-15 08:19:49 +0200

    setup

---
+++ src/main/java/com/bbdsoftware/estatemanagement/EstateManagementApplication.java
@@ -0,0 +1,13 @@
+package com.bbdsoftware.estatemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstateManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bbdsoftware/estatemanagement/EstateManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbdsoftware.estatemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
